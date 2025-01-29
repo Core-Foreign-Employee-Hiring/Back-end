4868b0170305c72e23ac4a87bff83cd0d003f9b0
22DT <qab3312@gmail.com>
2025-01-29 13:02:08 +0900

    [FIX] api request 설명 추가, 사용하지 않는 코드 삭제

--- src/main/java/com/core/foreign/api/member/service/MemberService.java
+++ src/main/java/com/core/foreign/api/member/service/MemberService.java
@@ -18,12 +18,10 @@ import com.core.foreign.common.response.ErrorStatus;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.validator.routines.EmailValidator;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -39,7 +37,6 @@ public class MemberService {
     private final PasswordEncoder passwordEncoder;
     private final EmailVerificationRepository emailVerificationRepository;
     private final PhoneNumberVerificationRepository phoneNumberVerificationRepository;
-    private final DuplicationUtil duplicationUtil;
     private final BusinessFieldUpdater businessFiledUpdater;
     private final BusinessFieldEntityRepository businessFieldEntityRepository;
     private final EmailService emailService;
@@ -240,91 +237,7 @@ public class MemberService {
 
     }
 
-    /**
-     * 지우기 아까움 ㅜㅜ
-     * @deprecated
-     *
 
-     */
-    @Transactional
-    public String sendVerificationEmail(Long memberId, String email, String token) {
-        // 이메일 형식 처리.
-
-        // Apache Commons EmailValidator 검증
-        if (!EmailValidator.getInstance().isValid(email)) {
-            throw new BadRequestException(ErrorStatus.VALIDATION_EMAIL_FORMAT_EXCEPTION.getMessage());
-        }
-
-        // 이메일 중복 처리.
-        boolean isToken = false;
-        boolean emailDuplicated = duplicationUtil.isEmailDuplicated(email);
-        if (emailDuplicated) { // 중복 o
-            // 중복 토근이 없으면.
-            if (token == null) {
-                throw new BadRequestException("중복된 이메일입니다.");
-            }
-
-            // 중복 토큰이 있으면
-            if (!jwtService.isTokenValid(token)) { // 토큰이 잘못되었을 경우.
-                log.info("토큰이 이상한데...");
-
-                /**
-                 * 현재 토큰에 문제가 있는 경우임.
-                 *
-                 * 1) 토큰 새로해서 발급해주기 <- 현재 유저가 어떤 토큰을 발급 받았는디 DB 에서 유지해줘야 함.
-                 * 2) 그냥 중복 이메일 처리 <- 실수로 뒤로가기 해결을 위해 토큰 방식을 사용하는데 목적에 전혀 맞지 않음
-                 *
-                 * 현재 2)이지만 나중에 리펙토링할 때 1) 로 수정해줘야 함.
-                 *
-                 */
-                throw new BadRequestException("중복된 이메일입니다.");
-            }
-            String duplicationIdString = jwtService.extractDuplicationId(token).get();
-            Long duplicationId = Long.valueOf(duplicationIdString);
-
-            Optional<EmailDuplication> duplicationMail = duplicationUtil.getDuplicationMail(duplicationId);
-
-            if (duplicationMail.isEmpty()) {
-                /**
-                 * token에 있는 메일이 DB 에 없을 경우 <- 스케줄링 잘못돌린 경우 같은데 token 의 수명이 스케줄링 주기보단 짦아야겠따.
-                 */
-
-                log.info("token 의 메일이 DB에 없음.");
-
-                throw new BadRequestException("중복된 이메일입니다.");
-            }
-
-            EmailDuplication emailDuplication = duplicationMail.get();
-            String tokenEmail = emailDuplication.getEmail();
-
-            if(email.equals(tokenEmail)){
-                isToken = true;
-            }else{  // 다른 이메일로 변경을 신청한 거야. 이럴 경우 다른 이메일이 우선.
-                
-            }
-            
-            
-        }
-
-        // 중복 x 면
-        log.info("중복된 이메일이 아닙니다.");
-
-        // 이메일 코드 발송
-        emailService.sendVerificationEmail(email, LocalDateTime.now());
-
-        // 중복 토큰 발급.
-
-        String duplicationToken;
-        if (isToken) {
-            duplicationToken = token;
-        } else {
-            Long duplicationMailId = duplicationUtil.getDuplicationMailId(email);
-            duplicationToken = jwtService.createDuplicationToken(duplicationMailId);
-
-        }
-
-        return duplicationToken;
-    }
 
     @Transactional
     public void updateEmployerEmail(Long memberId, String email){
@@ -345,8 +258,6 @@ public class MemberService {
 
         member.updateEmail(email);
 
-        // email 중복 테이블에서 삭제.
-//        duplicationUtil.removeEmailDuplication(email);
     }
 
     @Transactional
@@ -368,8 +279,6 @@ public class MemberService {
 
         employer.updateCompanyEmail(email);
 
-        // email 중복 테이블에서 삭제.
-//        duplicationUtil.removeEmailDuplication(email);
     }
 
     @Transactional
@@ -431,53 +340,7 @@ public class MemberService {
     }
 
 
-    /**
-     * @deprecated
-     * *
-     * 이메일 중복을 확인합니다.
-     * 1) Member Table 에서 이미 사용 중인지
-     * 2) 누군가 이미 변경하기 위해 찜해 뒀음.
-     *
-     * 변경할 email 을 미리 찜해두는 것임.
-     *  ** 만약, 찜해두고 변경 하지 않을 경우 찜 해제해야 함. <- 이 부분 프론트랑 상의할 것 **
-     *  찜 해제하기 위해 프론트가 백엔드에게 알려줬음 좋겠음.
-     *  *
-     */
-    @Transactional
-    public boolean isDuplicateEmail(String email) {
-        // 1)
-        boolean b = memberRepository.existsByEmail(email);
-        if(b){
-            throw new BadRequestException("중복된 이메일입니다.");
-        }
 
-
-        duplicationUtil.validateEmailDuplication(email);
-
-
-        return true;
-    }
-
-    /**
-     *
-     *
-     * @deprecated
-     */
-
-    @Transactional
-    public boolean isDuplicatePhoneNumber(String phoneNumber) {
-        // 1)
-        boolean b = memberRepository.existsByPhoneNumber(phoneNumber);
-        if(b){
-            throw new BadRequestException("중복된 전화 번호입니다.");
-        }
-
-
-        duplicationUtil.validatePhoneNumberDuplication(phoneNumber);
-
-
-        return true;
-    }
 
     @Transactional
     public void updateBusinessFiledOfEmployer(Long employerId, List<BusinessField> newFields){
