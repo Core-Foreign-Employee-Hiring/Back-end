356d25c80274ab230c52814d9fb5d51d655256ac
rhkr8521 <rhkr8521@gmail.com>
2025-02-28 15:22:18 +0900

    [REFAC] 슬랙 웹훅 임시 중지

--- src/main/java/com/core/foreign/common/advice/ControllerExceptionAdvice.java
+++ src/main/java/com/core/foreign/common/advice/ControllerExceptionAdvice.java
@@ -33,7 +33,7 @@ public class ControllerExceptionAdvice {
     public ResponseEntity<ApiResponse> handleGlobalException(BaseException ex) {
         // BadRequest, NotFound가 아닌 BaseException 예외라면 슬랙 알림
         if (!(ex instanceof BadRequestException) && !(ex instanceof NotFoundException)) {
-            slackNotificationService.sendServerErrorMessage(ex.getMessage());
+            //slackNotificationService.sendServerErrorMessage(ex.getMessage());
         }
 
         return ResponseEntity.status(ex.getStatusCode())
@@ -77,7 +77,7 @@ public class ControllerExceptionAdvice {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse> handleAnyException(Exception e) {
         // 여기서는 BadRequest, NotFound 예외가 아닌 모든 예외이므로 Slack 알림 전송
-        slackNotificationService.sendServerErrorMessage(e.getMessage());
+        //slackNotificationService.sendServerErrorMessage(e.getMessage());
 
         log.error("[handleAnyException] 알 수 없는 오류 발생", e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
