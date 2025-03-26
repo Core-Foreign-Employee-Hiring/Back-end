457cecdb0c7f1323a0bb7ed137ad89fda7292dca
22DT <qab3312@gmail.com>
2025-03-26 22:54:32 +0900

    [FIX] 공고 지원 확인 api 지원 불가 성명 추가

--- src/main/java/com/core/foreign/api/recruit/controller/RecruitController.java
+++ src/main/java/com/core/foreign/api/recruit/controller/RecruitController.java
@@ -997,8 +997,10 @@ public class RecruitController {
     @Operation(summary = "피고용인 공고 지원 가능한지 판단. API (용범)",
             description = "피고용인 공고 지원 가능한지 판단. API" +
                     "<p>" +
-                    "200: 지원 가능" +
-                    "그 외: 지원 불가")
+                    "200: 지원 가능<br>" +
+                    "그 외: 지원 불가<br>" +
+                    "400: 고용인이 공고 지원, 중복 지원, 스펙 및 경럭 필수 항목 없음<br>" +
+                    "404: 공고 없음, 스펙 및 경력 미작성")
     @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "판단 성공"),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "400", description = "잘못된 요청입니다.")
